f5c7fc8917be62f5940ec6df1536325108f4dcc7
guitavano <tavano62@gmail.com>
2023-12-19 14:48:14 -0300

    remove incorrect character

--- routes/_middleware.tsx
+++ routes/_middleware.tsx
@@ -8,7 +8,7 @@ export async function handler(
   const url = new URL(req.url);
 
   try{
-    const redirectRecord = await Deno.resolveDns(`redirect.${url.host}}`, "TXT")
+    const redirectRecord = await Deno.resolveDns(`redirect.${url.host}`, "TXT")
 
     url.host = `${redirectRecord[0][0]}.${url.host}`
     return Response.redirect(url.href, 301);
